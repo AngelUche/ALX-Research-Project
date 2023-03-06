6123c073b6cf5072c138b9304f3838444538edd9
AngelUche <ucheorji9974@gmail.com>
2023-03-06 21:12:44 +0000

    createdfile structue for the files

---
+++ academic-perfomance-project/src/data/data.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  FaBehance,
+  FaFacebook,
+  FaLinkedin,
+  FaTwitter,
+  FaSketch,
+  FaHome,
+  FaUserFriends,
+  FaFolderOpen,
+  FaCalendarAlt,
+  FaWpforms,
+} from "react-icons/fa";
+
+export const links = [
+  {
+    id: 1,
+    url: "/",
+    text: "home",
+    icon: <FaHome />,
+  },
+  {
+    id: 2,
+    url: "/team",
+    text: "team",
+    icon: <FaUserFriends />,
+  },
+  {
+    id: 3,
+    url: "/projects",
+    text: "projects",
+    icon: <FaFolderOpen />,
+  },
+  {
+    id: 4,
+    url: "/calendar",
+    text: "calendar",
+    icon: <FaCalendarAlt />,
+  },
+  {
+    id: 5,
+    url: "/documents",
+    text: "documents",
+    icon: <FaWpforms />,
+  },
+];
